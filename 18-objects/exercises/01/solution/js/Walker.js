746f043c176183329465a2da5d037c80eabc5aec
Gyozo Miklos <nyosdroid@gmail.com>
2022-09-17 00:20:02 +0200

    class solution done

--- 18-objects/exercises/01/solution/js/Walker.js
+++ 18-objects/exercises/01/solution/js/Walker.js
@@ -1,1 +1,53 @@
+/*
+const Walker = {
+  name: 'John Doe',
+  state: 'dead',
+  walk(speedInMph) {
+    return `${this.name} the ${this.state} walks with ${speedInMph} mph!`;
+  },
+  eat(foodName) {
+    return `${this.name} the ${this.state} eats ${foodName}!`;
+  },
+  speak(sound) {
+    return `${this.name} the ${this.state} says ${sound}!`;
+  },
+};
+*/
+
+// using classes
+class Walker {
+  constructor(name = 'John Doe', state = 'dead') {
+    this.name = name;
+    this.state = state;
+  }
+
+  walk(speedInMph = 10) {
+    return `${this.name} the ${this.state} walks with ${speedInMph} mph!`;
+  }
+
+  eat(foodName = 'meat') {
+    return `${this.name} the ${this.state} eats ${foodName}!`;
+  }
+
+  speak(sound = 'grrrrrr') {
+    return `${this.name} the ${this.state} says ${sound}!`;
+  }
+
+  get firstName() {
+    return this.name.split(' ')[0];
+  }
+
+  set firstName(fn) {
+    this.name = `${fn} ${this.lastName}`;
+  }
+
+  get lastName() {
+    return this.name.split(' ')[1];
+  }
+
+  set lastName(ln) {
+    this.name = `${this.firstName} ${ln}`;
+  }
+}
+
 export default Walker;
